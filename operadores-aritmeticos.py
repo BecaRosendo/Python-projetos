f0770af5ec073dacddc01d5087bfbd584e931659
Beca Alves <rebekaa128@gmail.com>
2025-11-15 22:05:09 +0000

    Initial commit

---
+++ operadores-aritmeticos.py
@@ -0,0 +1,8 @@
+x = y = z = 0
+
+x = int(input("Digite um numero : "))
+y = int(input("Digite um numero :")) 
+
+z = x+y 
+
+print(" soma dos dois valores é", z)
---
+++ operadores-logicos.py
@@ -0,0 +1,24 @@
+idade = 15
+altura = 1.75
+
+resultado = (idade >= 18 ) and (altura >= 1.70)
+msg= "Pode participar do evento ?" + str ( resultado) 
+
+print ( msg)
+
+
+
+#Programa de dispero de alarme 
+
+porta = "f"
+janela= "f" 
+
+alarme =(porta== "a" )or (janela=="a")
+msg = " Alarme disparado ?"+str (alarme)
+print(msg)
+
+
+tem_dinheiro = True
+tem_dinheiro = not tem_dinheiro
+msg = " tem dinheiro ?"+str (tem_dinheiro)
+print(msg)
